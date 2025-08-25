164a148d7a44a7b4e95d1bc22db87ff744e5ddb2
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-08-26 00:32:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,23 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+   const prices = document.querySelectorAll('.price');
+            let total = 0;
+
+            prices.forEach(price => {
+                total += Number(price.textContent);
+            })
+
+            const table = document.querySelector("table");
+            const newRow = document.createElement("tr");
+            const newCell = document.createElement("td");
+
+            newCell.setAttribute("colspan", "2"); // span across both columns
+            newCell.textContent = `Total Price: Rs ${total}`;
+            newCell.style.fontWeight = "bold";
+
+            newRow.appendChild(newCell);
+            table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
